cdff3a9860e5e26d9eede8366e965c18cc1af5c6
Tomoaki Azuma <tomoro@rise-pat.com>
2018-06-22 21:20:53 +0900

    add fulltxt app

--- sql/create_publn_data.sql
+++ sql/create_publn_data.sql
@@ -1,4 +1,4 @@
-/*CREATE TABLE app_names (
+CREATE TABLE app_names (
   name VARCHAR(255) PRIMARY KEY
 ) ENGINE=Mroonga DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='default_tokenizer "TokenDelimit"';
 
@@ -17,15 +17,22 @@ CREATE TABLE ipc_tags (
 CREATE TABLE fi_tags (
   name VARCHAR(255) PRIMARY KEY
 ) ENGINE=Mroonga DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='default_tokenizer "TokenDelimit"';
-*/
+
 CREATE TABLE publn_data (
   id INT PRIMARY KEY AUTO_INCREMENT,
   appln_nr VARCHAR(15) UNIQUE,
+  type VARCHAR(5),
   publn_nr VARCHAR(15),
   reg_nr VARCHAR(15),
   filing_date DATE,
+  filing_year VARCHAR(4),
+  filing_month VARCHAR(2),
   pub_date DATE,
+  pub_year VARCHAR(4),
+  pub_month VARCHAR(2),
   reg_date DATE,
+  reg_year VARCHAR(4),
+  reg_month VARCHAR(2),
   nb_claim INTEGER,
   applicants TEXT COMMENT 'flags "COLUMN_VECTOR", type "app_names"',
   attorneys TEXT COMMENT 'flags "COLUMN_VECTOR", type "ator_names"',
@@ -36,13 +43,18 @@ CREATE TABLE publn_data (
   description LONGTEXT,
   claims LONGTEXT,
   abstract TEXT,
-  FULLTEXT INDEX (applicants) COMMENT 'table "app_names"',
-  FULLTEXT INDEX (attorneys) COMMENT 'table "ator_names"',
-  FULLTEXT INDEX (inventors) COMMENT 'table "inv_names"',
-  FULLTEXT INDEX (clsf) COMMENT 'table "ipc_tags"',
-  FULLTEXT INDEX (fi) COMMENT 'table "fi_tags"',
+  FULLTEXT INDEX app_index(applicants) COMMENT 'table "app_names"',
+  FULLTEXT INDEX att_index(attorneys) COMMENT 'table "ator_names"',
+  FULLTEXT INDEX inv_index(inventors) COMMENT 'table "inv_names"',
+  FULLTEXT INDEX clsf_index(clsf) COMMENT 'table "ipc_tags"',
+  FULLTEXT INDEX fi_index(fi) COMMENT 'table "fi_tags"',
   FULLTEXT INDEX (title),
   FULLTEXT INDEX (description),
   FULLTEXT INDEX (claims),
   FULLTEXT INDEX (abstract)
 ) ENGINE = Mroonga DEFAULT CHARSET utf8;
+
+CREATE TABLE publn_error (
+  xml_path TEXT,
+  message TEXT
+)
---
+++ sql/create_user.sql
@@ -0,0 +1,2 @@
+CREATE USER 'tomoro'@'localhost' IDENTIFIED BY [PASSWORD] 'tomo';
+GRANT ALL PRIVILEGES ON `patent2`.* TO 'tomoro'@'localhost';
